e86fc88f3fc13c8ee3222668f7b203cc4e863d13
minimartzz <48483819+minimartzz@users.noreply.github.com>
2020-06-04 23:17:12 +0800

    Add files via upload

---
+++ experience.js
@@ -0,0 +1,64 @@
+
+$('.njc-ssbut').click(function() {
+    $('.njc-img').animate({opacity: 1}, 'slow');
+    $('.njc-slideshow').animate({opacity: 0}, 'slow');
+    $('.njc-carousel-hidden').css('visibility', 'visible');
+    setTimeout(function() {$('.njc-ssbut').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.njc-ssbut2').css({visibility: 'visible'})}, 500);
+});
+
+$('.njc-ssbut2').click(function(){
+    $('.njc-img').animate({opacity: 0.25}, 'slow');
+    $('.njc-slideshow').animate({opacity: 1}, 'slow');
+    $('.njc-carousel-hidden').css('visibility', 'hidden');
+    setTimeout(function() {$('.njc-ssbut2').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.njc-ssbut').css({visibility: 'visible'})}, 500);
+});
+
+$('.sutd-ssbut').click(function() {
+    $('.sutd-img').animate({opacity: 1}, 'slow');
+    $('.sutd-slideshow').animate({opacity: 0}, 'slow');
+    $('.sutd-carousel-hidden').css('visibility', 'visible');
+    setTimeout(function() {$('.sutd-ssbut').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.sutd-ssbut2').css({visibility: 'visible'})}, 500);
+});
+
+$('.sutd-ssbut2').click(function(){
+    $('.sutd-img').animate({opacity: 0.25}, 'slow');
+    $('.sutd-slideshow').animate({opacity: 1}, 'slow');
+    $('.sutd-carousel-hidden').css('visibility', 'hidden');
+    setTimeout(function() {$('.sutd-ssbut2').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.sutd-ssbut').css({visibility: 'visible'})}, 500);
+});
+
+$('.zju-ssbut').click(function() {
+    $('.zju-img').animate({opacity: 1}, 'slow');
+    $('.zju-slideshow').animate({opacity: 0}, 'slow');
+    $('.zju-carousel-hidden').css('visibility', 'visible');
+    setTimeout(function() {$('.zju-ssbut').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.zju-ssbut2').css({visibility: 'visible'})}, 500);
+});
+
+$('.zju-ssbut2').click(function(){
+    $('.zju-img').animate({opacity: 0.25}, 'slow');
+    $('.zju-slideshow').animate({opacity: 1}, 'slow');
+    $('.zju-carousel-hidden').css('visibility', 'hidden');
+    setTimeout(function() {$('.zju-ssbut2').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.zju-ssbut').css({visibility: 'visible'})}, 500);
+});
+
+$('.army-ssbut').click(function() {
+    $('.army-img').animate({opacity: 1}, 'slow');
+    $('.army-slideshow').animate({opacity: 0}, 'slow');
+    $('.army-carousel-hidden').css('visibility', 'visible');
+    setTimeout(function() {$('.army-ssbut').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.army-ssbut2').css({visibility: 'visible'})}, 500);
+});
+
+$('.army-ssbut2').click(function(){
+    $('.army-img').animate({opacity: 0.25}, 'slow');
+    $('.army-slideshow').animate({opacity: 1}, 'slow');
+    $('.army-carousel-hidden').css('visibility', 'hidden');
+    setTimeout(function() {$('.army-ssbut2').css({visibility: 'hidden'})}, 500);
+    setTimeout(function() {$('.army-ssbut').css({visibility: 'visible'})}, 500);
+});
